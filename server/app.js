4cfcf2a95e0503a87ea2702648d3eac2dd4c0ecc
vibhasfl <vibhas.fl@gmail.com>
2019-02-25 20:23:14 +0530

    Added helmet manage security header

--- server/app.js
+++ server/app.js
@@ -4,12 +4,14 @@ import { connectMongo } from './config/mongoconnect'
 import { errorHandler } from './config/errorHandler'
 import bodyParser from 'body-parser'
 import jwt from 'express-jwt'
+import helmet from 'helmet'
 import { httpStatus } from './utils/httpStatus'
 import { AppError } from './utils/appError'
 import { secretCallback } from './utils/secretCallback'
 const app = express()
 
 app.use(bodyParser.json())
+app.use(helmet())
 app.use(jwt({ secret: secretCallback }).unless({ path: [ '/api/health-check', '/api/users', '/api/auth/login' ], requestProperty: 'auth' }))
 app.use('/api', Router)
 
