91911c67c59567a3b62ed12d492103aa288a866d
vibhasfl <vibhas.fl@gmail.com>
2019-03-26 15:47:24 +0530

    Added Comment for mysql test route

--- server/modules/users/user.routes.js
+++ server/modules/users/user.routes.js
@@ -9,6 +9,8 @@ const userRoutes = express.Router()
 userRoutes.get('/', asyncWrapper(users.index))
 userRoutes.post('/', validate(newuser), asyncWrapper(users.create))
 userRoutes.put('/:id', asyncWrapper(users.update))
+
+// This route uses mysql DB for demo purpose to just show how can one use mysql in this app
 userRoutes.get('/testmysqlroute', asyncWrapper(users.testMysql))
 
 export { userRoutes }
